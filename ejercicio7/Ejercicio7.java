19d90d19b30d02413a99a1ee12fb3e5df09b58d5
IsraRamos <116392477+Jisraman@users.noreply.github.com>
2024-09-13 02:19:47 -0600

    prueba ejercicio 7 agregada

---
+++ ejercicio7/Ejercicio7.java
@@ -0,0 +1,7 @@
+public class Ejercicio7{
+    public static void main(String[] args) {
+    Rectangulo r = new Rectangulo(3, 4);
+    System.out.println(r.area());
+    System.out.println(r.imprimePerimetro());
+    }
+}
